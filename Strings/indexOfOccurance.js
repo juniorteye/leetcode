877c58a839cf9988e15ed7aae00f2a40dfa684a7
juniorteye <juniorteyegyemah@gmail.com>
2024-08-27 15:00:00 +0000

    Including the indexofOccurance

---
+++ Strings/indexOfOccurance.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle); // Returns the first occurrence index or -1 if not found
+}
+
